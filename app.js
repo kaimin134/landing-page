571367d52779a44d30329005edfe16c549a3dbe5
Minh Chung <chungvungocminh@gmail.com>
2024-02-09 23:49:17 -0600

    add snap scroll

--- app.js
+++ app.js
@@ -1,63 +1,58 @@
-
-const clientHeight = document.getElementById('info').clientHeight;
-const clientWidth = document.getElementById('info').clientWidth;
-const clientHeight2 = document.getElementById('quote').clientHeight;
-const clientWidth2 = document.getElementById('quote').clientWidth;
+const clientHeight = document.getElementById("info").clientHeight;
+const clientWidth = document.getElementById("info").clientWidth;
+// const clientHeight2 = document.getElementById("quote").clientHeight;
+// const clientWidth2 = document.getElementById("quote").clientWidth;
 console.log(clientHeight);
 console.log(clientWidth);
 const info = document.querySelector(".info");
+const info2 = document.querySelector(".info2");
 const background = document.createElement("div");
+const background2 = document.createElement("div");
 background.classList.add("background");
+background2.classList.add("background2");
 info.appendChild(background);
-function createGrid (x) {
-  
-        for (let i = 0; i < x*x; i++) {
-            let square = document.createElement('div');
-            square.setAttribute("id", "grid");
-            square.style.height = `${clientWidth / 20}px`;
-            square.style.width = `${clientWidth / 20}px`;
-            background.appendChild(square);
-        }
-    
-};
-/*
-function createGrid2 (x) {
-  
-    for (let i = 0; i < x*x; i++) {
-        let square2 = document.createElement('div');
-        square2.setAttribute("id", "grid2");
-        square2.style.height = `${clientWidth2 / 20}px`;
-        square2.style.width = `${clientWidth2 / 20}px`;
-        background.appendChild(square2);
-    }
-
-};
-*/
+info2.appendChild(background2);
+function createGrid(x) {
+  for (let i = 0; i < x * x; i++) {
+    let square = document.createElement("div");
+    square.setAttribute("id", "grid");
+    square.style.height = `${clientWidth / 20}px`;
+    square.style.width = `${clientWidth / 20}px`;
+    background.appendChild(square);
+  }
+}
 createGrid(10);
 
-/*createGrid2(10);*/
+function createGrid2(x) {
+  for (let i = 0; i < x * x; i++) {
+    let square2 = document.createElement("div");
+    square2.setAttribute("id", "grid2");
+    square2.style.height = `${clientWidth / 20}px`;
+    square2.style.width = `${clientWidth / 20}px`;
+    background2.appendChild(square2);
+  }
+}
 
+createGrid2(10);
 
 const grids = document.querySelectorAll("#grid");
 grids.forEach((grid) => {
-    grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
-    });
+  grid.addEventListener("mouseover", () => {
+    grid.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
+  });
 });
 
-
 function random() {
-    return Math.floor(Math.random() * 256) + 150;
+  return Math.floor(Math.random() * 256) + 150;
 }
 
 const grids2 = document.querySelectorAll("#grid2");
 grids2.forEach((grid) => {
-    grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = `rgb(${random2()}, ${random2()}, ${random2()})`;
-    });
+  grid.addEventListener("mouseover", () => {
+    grid.style.backgroundColor = `rgb(${random2()}, ${random2()}, ${random2()})`;
+  });
 });
 
-
 function random2() {
-    return Math.floor(Math.random() * 150);
+  return Math.floor(Math.random() * 150);
 }
